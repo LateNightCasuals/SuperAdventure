1d4de0c1b5c05051eb65bde59a8b65e3d808bf00
Corey <Corey@COREY-PC>
2018-02-10 13:13:41 +0000

    Initialised a basic player

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Engine {
-	public class Player {
+namespace Engine
+{
+	public class Player
+	{
 		public int CurrentHitPoints { get; set; }
 		public int MaximumHitPoints { get; set; }
 		public int Gold { get; set; }
