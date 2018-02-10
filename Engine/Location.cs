7a86b80212bc6c80dbfada8f52638cd11bd5052a
Corey <Corey@COREY-PC>
2018-02-10 13:41:41 +0000

    Added constructors to classes (with inheritence from base classes) and tidied up initialisation.

--- Engine/Location.cs
+++ Engine/Location.cs
@@ -8,6 +8,12 @@ namespace Engine
 {
 	public class Location
 	{
+		public Location(int id, string name, string description)
+		{
+			ID = id;
+			Name = name;
+			Description = description;
+		}
 		public int ID { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; }
